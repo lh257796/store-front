0a144b739f1bda71fef2021c566a34ffa2846385
Han <hl2341@nyu.edu>
2023-11-01 14:08:45 -0400

    attempting deploy, changed usecart hook

--- hooks/use-cart.ts
+++ hooks/use-cart.ts
@@ -1,8 +1,8 @@
-import { create } from "zustand";
+import { create } from 'zustand';
+import { toast } from 'react-hot-toast';
 import { persist, createJSONStorage } from "zustand/middleware";
-import toast from "react-hot-toast";
 
-import { Product } from "@/types";
+import { Product } from '@/types';
 
 interface CartStore {
   items: Product[];
@@ -12,29 +12,28 @@ interface CartStore {
 }
 
 const useCart = create(
-  persist<CartStore>(
-    (set, get) => ({
-      items: [],
-      addItem: (data: Product) => {
-        const currentItems = get().items;
-        const existingItem = currentItems.find((item) => item.id === data.id);
-        if (existingItem) {
-          return toast("Item already in cart!");
-        }
-        set({ items: [...get().items, data] });
-        toast.success("Item added to cart!");
-      },
-      removeItem: (id: string) => {
-        set({ items: [...get().items.filter((item) => item.id !== id)] });
-        toast.success("Item removed from cart!");
-      },
-      removeAll: () => set({ items: [] }),
-    }),
-    {
-      name: "cart-storage",
-      storage: createJSONStorage(() => localStorage),
+  //@ts-ignore
+  persist<CartStore>((set, get) => ({
+  items: [],
+  addItem: (data: Product) => {
+    const currentItems = get().items;
+    const existingItem = currentItems.find((item) => item.id === data.id);
+
+    if (existingItem) {
+      return toast('Item already in cart.');
     }
-  )
-);
+
+    set({ items: [...get().items, data] });
+    toast.success('Item added to cart.');
+  },
+  removeItem: (id: string) => {
+    set({ items: [...get().items.filter((item) => item.id !== id)] });
+    toast.success('Item removed from cart.');
+  },
+  removeAll: () => set({ items: [] }),
+}), {
+  name: 'cart-storage',
+  storage: createJSONStorage(() => localStorage)
+}));
 
 export default useCart;
